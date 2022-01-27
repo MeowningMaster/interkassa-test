37bd6dcc5d54d766e18493b7a32fe4425f3bf993
MeowningMaster <meowningmaster@gmail.com>
2022-01-27 19:36:03 +0300

    fix constants

--- src/consts.ts
+++ src/consts.ts
@@ -1,20 +1,22 @@
 import "i/dotenv/load.ts";
-const { PORT, THIS_HOST, INTERKASSA_SECRET_KEY, INTERKASSA_TEST_KEY } = Deno.env.toObject();
+const { PORT, THIS_HOST, INTERKASSA_SECRET_KEY, INTERKASSA_TEST_KEY } = Deno.env
+  .toObject();
 
-export const interactionPath = '/interaction';
+export const interactionPath = "/interaction";
 
 export const consts = {
-    PORT,
-    INTERKASSA: {
-        KEYS: {
-            SECRET: INTERKASSA_SECRET_KEY,
-            TEST: INTERKASSA_TEST_KEY,
-        },
-        URLS: {
-            INTERACTION: `${THIS_HOST}${interactionPath}`,
-            SUCCESS: `${THIS_HOST}/success`,
-            FAIL: `${THIS_HOST}/fail`,
-            PENDING: `${THIS_HOST}/pending`
-        }
-    }
-}
+  PORT,
+  THIS_HOST,
+  INTERKASSA: {
+    KEYS: {
+      SECRET: INTERKASSA_SECRET_KEY,
+      TEST: INTERKASSA_TEST_KEY,
+    },
+    URLS: {
+      INTERACTION: `${THIS_HOST}${interactionPath}`,
+      SUCCESS: `${THIS_HOST}/success`,
+      FAIL: `${THIS_HOST}/fail`,
+      PENDING: `${THIS_HOST}/pending`,
+    },
+  },
+};
