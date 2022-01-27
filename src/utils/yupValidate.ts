37e45bd98c1a931cdaf1243aa0ad2b2575a97399
MeowningMaster <meowningmaster@gmail.com>
2022-01-27 19:37:28 +0300

    fix yup validation

--- src/utils/yupValidate.ts
+++ src/utils/yupValidate.ts
@@ -1,9 +1,12 @@
 import yup from "i/yup.ts";
 
 const validateOptions = {
-	abortEarly: false,
+  abortEarly: false,
 };
 
-export const validate = async <T1, T2, T3>(data: T1, schema: yup.BaseSchema<T1, T2, T3>): T3 => {
-	return await schema.validate(data, validateOptions);
+export const validate = <T1, T2, T3>(
+  data: T1,
+  schema: yup.BaseSchema<T1, T2, T3>,
+): Promise<T3> => {
+  return schema.validate(data, validateOptions);
 };
