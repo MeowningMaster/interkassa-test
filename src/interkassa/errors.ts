3f55a6e7a90b7cdd1c862804897b832eb02aca9e
MeowningMaster <meowningmaster@gmail.com>
2022-01-27 19:35:53 +0300

    fix errors

--- src/interkassa/errors.ts
+++ src/interkassa/errors.ts
@@ -1,11 +1,34 @@
-export class InterkassaPaymentAlertValidationError extends Error {
-    constructor(message?: string) {
-        super(message ?? "Interkassa payment alert validation error");
-    }
+import { ValidationError as YupValidationError } from "i/yup.ts";
+
+export class InterkassaPaymentAlertError extends Error {
+  constructor(message?: string) {
+    super(message ?? "Uspecified error");
+  }
 }
 
-export class InvalidInterkassaIpError extends Error {
-    constructor(ip: string) {
-        super(`Invalid interkassa ip: ${ip}`);
-    }
+export class InterkassaPaymentAlertValidationError
+  extends InterkassaPaymentAlertError {
+  constructor(error: YupValidationError, data: unknown) {
+    super(`Validation failed\ndata:${data}\nerror:${error.message}`);
+  }
+}
+
+export class InterkassaPaymentAlertIncorrectDataError
+  extends InterkassaPaymentAlertError {
+  constructor(key: string, ourValue: string, theirValue: string) {
+    super(`Incorrect ${key} (our ${ourValue} != their ${theirValue})`);
+  }
+}
+
+export class InterkassaPaymentAlertSignatureError
+  extends InterkassaPaymentAlertError {
+  constructor() {
+    super("Signature error");
+  }
+}
+
+export class InterkassaPaymentAlertIpError extends InterkassaPaymentAlertError {
+  constructor(ip: string) {
+    super(`Invalid ip: ${ip}`);
+  }
 }
